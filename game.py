66805457d4d67f829dfc665a123a49e5033db41f
flin16 <flin16@illinois.edu>
2025-05-23 10:41:43 -0500

    Auto commit

--- game.py
+++ game.py
@@ -1,7 +1,10 @@
 import random, time
 from typing import Optional
 import env, lib
+
 random.seed(time.time())
+
+
 class Card:
     def __init__(self, suit, val):
         self.suit = suit
@@ -9,10 +12,14 @@ class Card:
 
     def __str__(self):
         value = self.value
-        if value == 1: value = 'A'
-        elif value == 11: value = 'J'
-        elif value == 12: value = 'Q'
-        elif value == 13: value = 'K'
+        if value == 1:
+            value = "A"
+        elif value == 11:
+            value = "J"
+        elif value == 12:
+            value = "Q"
+        elif value == 13:
+            value = "K"
         return f"{value} of {self.suit}"
 
     def __eq__(self, other):
@@ -20,7 +27,8 @@ class Card:
             value = other.value
         else:
             value = other
-        return self.value == value 
+        return self.value == value
+
 
 class Deck:
     def __init__(self):
@@ -33,7 +41,7 @@ class Deck:
                 self.cards.append(Card(s, v))
         # input jokers
         for i in range(2):
-            self.cards.append(Card('🤡', 0))
+            self.cards.append(Card("🤡", 0))
 
     def show(self):
         for c in self.cards:
@@ -45,42 +53,45 @@ class Deck:
             self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
 
     def drawCard(self):
-        if not self.cards: return
+        if not self.cards:
+            return
         return self.cards.pop()
 
 
 class Player:
     drew = False
+
     def __init__(self, name):
         self.name = name
         self.hand = []
 
-
     def draw(self, deck):
         ans = deck.drawCard()
-        if not ans: return
+        if not ans:
+            return
         self.hand.append(ans)
         return ans
 
     def show_hand(self):
         return [str(card) for card in self.hand]
-    
+
     def __str__(self):
         return self.name
 
+
 class Game:
     length = 0
-    reversed = False 
+    reversed = False
     highest = None
 
     def __str__(self):
-        return f'''\n player: {self.current_player} \n
+        return f"""\n player: {self.current_player} \n
             player_list: {[str(player) for player in self.players]} \n
             cards: {[len(player.hand) for player in self.players]} \n
             highest: {self.highest} \n
             current_player: {self.current_player} \n
             starter: {self.starter} \n
-            reversed: {self.reversed}\n'''
+            reversed: {self.reversed}\n"""
 
     def find_player(self, name) -> Optional[Player]:
         for player in self.players:
@@ -96,44 +107,44 @@ class Game:
             self.players.append(Player(player))
 
     def start(self):
-        'Start the game'
+        "Start the game"
         for player in self.players:
             for i in range(env.init_card_per_player):
                 player.draw(self.deck)
-        'Decide the player who starts the game'
+        "Decide the player who starts the game"
         self.current_id = random.randint(0, len(self.players) - 1)
         self.current_player = self.players[self.current_id]
         self.starter = self.current_player
 
-
     def clear_round(self):
-        'Clear the round'
+        "Clear the round"
         self.highest = None
         self.starter = self.current_player
 
     def compare(self, comb, highest):
-        'return True if power1 > power2 in current sense'
-        if not highest: return True, ''
-        message = ''
+        "return True if power1 > power2 in current sense"
+        if not highest:
+            return True, ""
+        message = ""
         power1 = comb.power
         power2 = highest.power
-        if power1 == 57: 
-            return True, 'Grothendieck Prime'
+        if power1 == 57:
+            return True, "Grothendieck Prime"
         if power2 == 57:
-            return False, 'Grothendieck Prime'
+            return False, "Grothendieck Prime"
         if power1 == 1729:
             self.reversed = not self.reversed
-            return True, 'Ramanujan Number'
+            return True, "Ramanujan Number"
         if comb.length != highest.length:
             res = False
-            return res, 'Cards length not equal'
+            return res, "Cards length not equal"
         elif self.reversed:
-            res =  power1 < power2
+            res = power1 < power2
         else:
-            res =  power1 > power2
+            res = power1 > power2
         if not res:
-            message = 'smaller' if self.reversed else 'greater'
-            message = f'Card power should be {message} than the previous one, which is {power2}'
+            message = "smaller" if self.reversed else "greater"
+            message = f"Card power should be {message} than the previous one, which is {power2}"
         return res, message
 
     def empty(self):
@@ -143,57 +154,63 @@ class Game:
         return True
 
     def last_player(self):
-        'Return the last player'
-        return self.players[(self.current_id + len(self.players) - 1) % len(self.players)]
+        "Return the last player"
+        return self.players[
+            (self.current_id + len(self.players) - 1) % len(self.players)
+        ]
 
     def play(self, statement):
-        'Let current player play'
+        "Let current player play"
         comb = lib.get_cards(statement)
-        if not isinstance(comb, lib.Comb): return None, 'Expression illegal'
-        numbers= comb.numbers
-        cards = [Card('Spades', int(number)) for number in numbers]
-        punish = f', punish {len(cards)}'
-        if not comb.success: return None, 'Expression illegal' + punish
-        if not cards: return None, f'Cards illegal' + punish
+        if not isinstance(comb, lib.Comb):
+            return None, "Expression illegal"
+        numbers = comb.numbers
+        cards = [Card("Spades", int(number)) for number in numbers]
+        punish = f", punish {len(cards)}"
+        if not comb.success:
+            return None, "Expression illegal" + punish
+        if not cards:
+            return None, f"Cards illegal" + punish
         if self.current_player == self.starter:
             self.clear_round()
         ok, message = self.compare(comb, self.highest)
         if not ok:
-            return False, message  + punish
+            return False, message + punish
         hand_copy = self.current_player.hand.copy()
         for card in cards:
             if card not in hand_copy:
                 # for i in range(len(cards)):
-                    # self.current_player.draw(self.deck)
-                return False, f'Card {card} not in hand' + punish
+                # self.current_player.draw(self.deck)
+                return False, f"Card {card} not in hand" + punish
             hand_copy.remove(card)
         for card in cards:
-            self.current_player.hand.remove(card) 
+            self.current_player.hand.remove(card)
         self.starter = self.current_player
-        if not self.empty(): self.skip()
+        if not self.empty():
+            self.skip()
         self.highest = comb
-        return True, f'Cards played by {self.current_player} successfully'
-            
-    def draw(self, force = False):
-        'Draw a card for the current player'
+        return True, f"Cards played by {self.current_player} successfully"
+
+    def draw(self, force=False):
+        "Draw a card for the current player"
         if force or not self.current_player.drew:
             ans = self.current_player.draw(self.deck)
-            if not ans: return False, 'No card left'
+            if not ans:
+                return False, "No card left"
             self.current_player.drew = True
-            return True, f'{self.current_player} drew a card'
-        return False, f'{self.current_player} already drew a card'
+            return True, f"{self.current_player} drew a card"
+        return False, f"{self.current_player} already drew a card"
 
     def skip(self):
-        'Skip the current player'
+        "Skip the current player"
         self.current_id = (self.current_id + 1) % len(self.players)
         self.current_player.drew = False
         self.current_player = self.players[self.current_id]
         if len(self.current_player.hand) == 0:
             self.skip()
 
-        
-if __name__ == '__main__':
-    g = Game(['Alice', 'Bob', 'Charlie'])
+
+if __name__ == "__main__":
+    g = Game(["Alice", "Bob", "Charlie"])
     g.start()
-    print(g.compare(lib.get_cards('5 6 3'), lib.get_cards('13')))
-    
+    print(g.compare(lib.get_cards("5 6 3"), lib.get_cards("13")))
